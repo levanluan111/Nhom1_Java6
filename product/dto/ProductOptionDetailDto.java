7db9ddd972926b85709bcf8d75b376b89566b578
levanluan111 <levanluan782@gmail.com>
2025-04-25 22:36:13 +0700

    update file auth, cart, config, controller, file, payment,product

---
+++ product/dto/ProductOptionDetailDto.java
@@ -0,0 +1,23 @@
+package com.shop.clothing.product.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductOptionDetailDto extends ProductOptionDto {
+    private ProductBriefDto product;
+    private int quantity=0;
+
+    public int getFinalPrice() {
+        return (int) (this.getProduct().getPrice() * (100.0 - this.getProduct().getDiscount()) / 100);
+    }
+
+    public String getFinalPriceDisplay() {
+        return String.format("%,dđ", getFinalPrice());
+    }
+
+    public String getTotalDisplay(){
+        return String.format("%,dđ", getFinalPrice()*quantity);
+    }
+}
